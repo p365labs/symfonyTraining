a970863e1cd5410201ddb7ab4b525db745d5ade2
Federico Panini <federico.panini@drivek.com>
2018-11-17 09:22:36 +0100

    add Controller

---
+++ src/AppBundle/Controller/AccountController.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Account;
+use AppBundle\Entity\Contact;
+use AppBundle\Form\AccountType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AccountController extends Controller
+{
+
+}
