6f8b29f9f3db0c81791151c38fc7dd8bffc924ab
Federico Panini <federico.panini@drivek.com>
2018-11-17 09:12:47 +0100

    Symfony setup

---
+++ con.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker exec -it doctrine_training_php bin/console $1
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where compiled assets will be stored
+    .setOutputPath('web/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('app_css', './assets/css/global.scss')
+    .autoProvidejQuery()
+    .enableSassLoader()
+    //.enableVersioning()
+
+
+;
+
+module.exports = Encore.getWebpackConfig();
+
